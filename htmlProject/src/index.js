12c481e5cbb9a820577fab49606a04db72d46d95
Weasley <weasleyqi@gmail.com>
2015-08-18 08:05:37 +0800

    javascript
    js函数

--- htmlProject/src/index.js
+++ htmlProject/src/index.js
@@ -1,3 +1,23 @@
 /**
  * Created by Administrator on 2015/8/18.
  */
+function A(){
+    this.sayhehe = function(){
+        console.log("hehe jikexueyuan");
+    };
+}
+
+A.prototype.sayHello = function(){
+    console.log("hello js");
+}
+
+A.sayHi = function(){
+    console.log("hello jsaaaa");
+}
+
+var a = new A();
+a.sayHello();
+A.sayHi();
+
+function B (){}
+B.prototype = new A();
