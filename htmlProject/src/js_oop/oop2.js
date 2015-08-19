81f2a5b38ecffcd85c883ae255aa68ee76a75ee0
weasley <weasleyqi@gmail.com>
2015-08-20 00:14:37 +0800

    wall fall

--- htmlProject/src/js_oop/oop2.js
+++ htmlProject/src/js_oop/oop2.js
@@ -1,6 +1,27 @@
 /**
  * Created by Administrator on 2015/8/19.
  */
-function people(){
+(function(){
+    function Person(name){
+            var _this = {};
+            _this.name = name;
+            _this.sayHello = function () {
+                alert("P hello >> "+_this.name);
+            }
+        return _this;
+    }
+    window.Person = Person;
+}());
 
+function Teacher(name){
+    var _this = Person(name);
+    var suSay = _this.sayHello;
+    _this.sayHello = function(){
+        suSay.call(_this);
+        alert("T Hello >>>"+_this.name);
+    }
+    return _this;
 }
+
+var t = new Teacher("weasley");
+t.sayHello();
